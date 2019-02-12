f0b79f424c71fcbb89defc894dabf8f406828ac4
SeamasShih <seamas0716@gmail.com>
2019-02-12 09:46:53 +0800

    Initial commit

---
+++ app/src/main/java/com/seamas/colorhintbar/MainActivity.java
@@ -0,0 +1,20 @@
+package com.seamas.colorhintbar;
+
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ColorHintBar chb = findViewById(R.id.chb);
+        chb.setItemAmount(3);
+        chb.setBackgroundHintColor(Color.BLUE);
+        chb.setOnClickListener(v -> chb.setSite((chb.getSite() + 1) % 3));
+    }
+}
